b242a9cfdc271218f7619d98419ae0f92128d014
Odunlola <odunolaodunlami@gmail.com>
2023-04-01 15:49:51 -0500

    add new

--- solution_code/controllers/fruits.js
+++ solution_code/controllers/fruits.js
@@ -2,18 +2,27 @@ const express = require('express');
 const router = express.Router();
 const startFruits = require('../db/fruitSeedData.js')
 const Fruit = require('../models/fruit.js')
-
+//post
 router.post('/', async (req, res) => {
 	const fruit = await Fruit.create(req.body);
 	res.send(fruit);
 });
 
-// Index ...show all fruits
-router.get('/', async (req, res) => {
-	const fruits = await Fruit.find({});
-	// res.send(fruits);
-	res.render('fruits/index.ejs', {fruits})
-});
+// New
+router.get('/new', (req, res) => {
+	res.render("fruits/new.ejs")
+   })
+   
+   // Index...show all fruits
+   router.get('/', async (req, res) => {
+	   // wait or this to complete
+	   // Fruit.find() is a Promise
+	   // Promise is resolved or rejected
+	   const fruits = await Fruit.find({});
+	   // then run the next line of code
+	   // res.send(fruits);
+	   res.render("fruits/index.ejs", {fruits});
+   });
 
 // Seed
 router.get('/seed', async (req, res) => {
