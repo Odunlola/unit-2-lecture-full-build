b99f0f1ddf327a72cbbdaedb41d3862fc735a252
Odunlola <odunolaodunlami@gmail.com>
2023-04-08 13:59:01 -0500

    update

--- solution_code/controllers/fruits.js
+++ solution_code/controllers/fruits.js
@@ -2,27 +2,36 @@ const express = require('express');
 const router = express.Router();
 const startFruits = require('../db/fruitSeedData.js')
 const Fruit = require('../models/fruit.js')
-//post
+
+// Post
 router.post('/', async (req, res) => {
+	console.log(req.body)
+	req.body.readyToEat = req.body.readyToEat === 'on' ? true : false;
 	const fruit = await Fruit.create(req.body);
-	res.send(fruit);
+	res.redirect('/fruits');
 });
 
 // New
 router.get('/new', (req, res) => {
-	res.render("fruits/new.ejs")
-   })
-   
-   // Index...show all fruits
-   router.get('/', async (req, res) => {
-	   // wait or this to complete
-	   // Fruit.find() is a Promise
-	   // Promise is resolved or rejected
-	   const fruits = await Fruit.find({});
-	   // then run the next line of code
-	   // res.send(fruits);
-	   res.render("fruits/index.ejs", {fruits});
-   });
+ res.render("fruits/new.ejs")
+})
+
+// /fruits/123/edit
+router.get('/:id/edit', async (req, res) => {
+	const fruit = await Fruit.findById(req.params.id);
+	res.render("fruits/edit.ejs", {fruit})
+})
+
+// Index...show all fruits
+router.get('/', async (req, res) => {
+	// wait or this to complete
+	// Fruit.find() is a Promise
+	// Promise is resolved or rejected
+	const fruits = await Fruit.find({});
+	// then run the next line of code
+	// res.send(fruits);
+	res.render("fruits/index.ejs", {fruits, user:re});
+});
 
 // Seed
 router.get('/seed', async (req, res) => {
@@ -31,11 +40,11 @@ router.get('/seed', async (req, res) => {
 	res.redirect('/fruits');
 });
 
-// Show ..show one fruit
+// Show...show one fruit
 router.get('/:id', async (req, res) => {
 	const fruit = await Fruit.findById(req.params.id);
 	// res.send(fruit);
-	res.render("fruits/show.ejs",{fruit})
+	res.render("fruits/show.ejs", {fruit})
 });
 
 // Delete
@@ -46,10 +55,12 @@ router.delete('/:id', async (req, res) => {
 
 // Update
 router.put('/:id', async (req, res) => {
-	const fruit = await Fruit.findByIdAndUpdate(req.params.id, req.body, {
+	const id = req.params.id;
+	req.body.readyToEat = req.body.readyToEat === 'on' ? true : false;
+	const fruit = await Fruit.findByIdAndUpdate(id, req.body, {
 		new: true,
 	});
-	res.send(fruit);
+	res.redirect('/fruits');
 });
 
 module.exports = router;
--- solution_code/server.js
+++ solution_code/server.js
@@ -1,14 +1,35 @@
 // Dependencies
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan'); 
 const methodOverride = require('method-override');
 const app = express();
-require('dotenv').config();
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
+
+app.use(
+    session({
+        // The store needs to know that it's a mongo database and it needs access to the databases' connection
+        store: MongoStore.create({ 
+            mongoUrl: process.env.DATABASE_URL 
+        }),
+        // The secret ensures it's not some outside attack and it signs every session
+        secret: "super secret",
+        // No resaving the same same session and saving unitialized sessions
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 30
+            // This set the cookie to last for 30 days because it's in milliseconds
+        }
+    }),
+)
 /////////////////////////////////////////////////////
-// Middleware
+// Middleware  req => middleware => res
 /////////////////////////////////////////////////////
-app.use(morgan("tiny")) //logging
+app.set('view engine', 'ejs')
+app.use(morgan("tiny")) //logging// 
 app.use(methodOverride("_method")) // override for put and delete requests from forms
 app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
 app.use(express.static("public")) // serve files from public statically
@@ -18,7 +39,9 @@ app.get('/', (req, res) => {
 })
 
 const fruitsController = require('./controllers/fruits');
+const userController = require('./controllers/users')
 app.use('/fruits', fruitsController);
+app.use('', userController);
 
 // Listener
 app.listen(process.env.PORT, () =>
